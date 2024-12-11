a493edef9cdd27dd1f5b29391511399f0c926a1d
Rodrigo-Nunes-a22302691 <a22302691@alunos.ulht.pt>
2024-12-11 16:18:50 +0000

    Paginas

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -5,7 +5,7 @@ const Card = ({ produto }) => {
   return (
     <div className={styles.produto}>  {/* Usando a classe como uma propriedade do objeto styles */}
       <h3>{produto.title}</h3>
-      <img src={produto.image} alt={`Imagem de ${produto.title}`} />
+      <img className={styles.imagem} src={produto.image} alt={`Imagem de ${produto.title}`} />
       <p className={styles.preco}>Custo total: {produto.price}€</p>
       <p className={styles.descricao}>{produto.description}</p>
       <p className={styles.rating}>{produto.rating.rate} / 5.0 <span className={styles.star}>★</span></p>
